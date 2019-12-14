f2a9e32fcdcbbef3823a59bbc57559456dbf9788
Hristian <hristian.stoilov@students.fhnw.ch>
2019-12-14 11:41:32 +0100

    whatever

---
+++ grails-app/services/exercise/CrudMessageService.groovy
@@ -0,0 +1,45 @@
+package exercise
+
+import grails.CRUD
+import groovy.transform.CompileStatic
+import org.springframework.context.MessageSource
+
+@SuppressWarnings('GrailsStatelessService')
+@CompileStatic
+class CrudMessageService {
+
+    MessageSource messageSource
+
+    String message(CRUD crud, String domainName, Long domainId, Locale locale) {
+        messageSource.getMessage(codeByCrud(crud),
+                [domainName, domainId] as Object[],
+                defaultMessageByCrud(crud),
+                locale)
+    }
+
+    String defaultMessageByCrud(CRUD crud) {
+        switch (crud) {
+            case CRUD.NOT_FOUND:
+                'Not Found'
+            case CRUD.CREATE:
+                'Created'
+            case CRUD.UPDATE:
+                'Updated'
+            case CRUD.DELETE:
+                'Deleted'
+        }
+    }
+
+    String codeByCrud(CRUD crud) {
+        switch (crud) {
+            case CRUD.NOT_FOUND:
+                'default.not.found.message'
+            case CRUD.CREATE:
+                'default.created.message'
+            case CRUD.UPDATE:
+                'default.updated.message'
+            case CRUD.DELETE:
+                'default.deleted.message'
+        }
+    }
+}
---
+++ src/main/groovy/grails/CRUD.groovy
@@ -0,0 +1,8 @@
+package grails
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+enum CRUD {
+    CREATE, DELETE, UPDATE, NOT_FOUND
+}
